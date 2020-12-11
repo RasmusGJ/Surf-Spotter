1b3492e6f76950d92a7f7a349b44e22aad5cd2e6
asgeucl <64077147+asgeucl@users.noreply.github.com>
2020-12-11 12:37:54 +0100

    ViewComponent=?)=

---
+++ surf spotter dot net core/surf spotter dot net core/ViewComponents/SpotCounterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using surf_spotter_dot_net_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace surf_spotter_dot_net_core.ViewComponents
+{
+    [ViewComponent]
+    public class SpotCounterViewComponent
+    {
+        private readonly IdentityDataContext _db;
+        public SpotCounterViewComponent(IdentityDataContext db)
+        {
+            _db = db;
+        }
+        public string Invoke()
+        {
+            return _db.Spots.Count().ToString();
+        }
+    }
+}
